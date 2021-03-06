5c0076d7f3f76d8e6a7f820683d3b9c724cc4ba2
hyunsu00 <hyun955807@naver.com>
2021-03-07 02:30:44 +0900

    이것도 테스트

--- pybo/views.py
+++ pybo/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    msg = "안녕하세요 pybo에 오신것을 환영합니다."
+    msg = "안녕하세요 pybo에 오신것을 환영합니다." + "\n이건 추가 메시지 입니다."
     return HttpResponse(msg)
