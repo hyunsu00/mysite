1336cc89cb3338b5c678b624edb08ad6c817cd60
hyunsu00 <hyun955807@naver.com>
2021-03-14 23:49:20 +0900

    아마존 Lightsail 용 IP 추가

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jc#c^%pkuyh8!#gi5cz(9ub-gbdl#w5t-ri2l-x*awb)&tnhax'
+SECRET_KEY = "jc#c^%pkuyh8!#gi5cz(9ub-gbdl#w5t-ri2l-x*awb)&tnhax"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,54 +31,54 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pybo.apps.PyboConfig',  # 추가된 App
-    'common.apps.CommonConfig',  # 추가된 App
-    'django.contrib.admin',
-    'django.contrib.auth',  # 로그인 · 로그아웃
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "pybo.apps.PyboConfig",  # 추가된 App
+    "common.apps.CommonConfig",  # 추가된 App
+    "django.contrib.admin",
+    "django.contrib.auth",  # 로그인 · 로그아웃
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],  # HTML 파일
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],  # HTML 파일
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -88,16 +88,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'  # 언어
+LANGUAGE_CODE = "ko-kr"  # 언어
 
-TIME_ZONE = 'Asia/Seoul'  # 시간대
+TIME_ZONE = "Asia/Seoul"  # 시간대
 
 USE_I18N = True
 
@@ -119,38 +119,31 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
 
 # 로그인/로그아웃 성공후 이동하는 URL
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # 디버그 툴바 설정
 if DEBUG:
-    INTERNAL_IPS = ('127.0.0.1',)
+    INTERNAL_IPS = ("127.0.0.1",)
+    INSTALLED_APPS += ("debug_toolbar",)
+    MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 
-    INSTALLED_APPS += (
-        'debug_toolbar',
-    )
+ALLOWED_HOSTS += ("127.0.0.1", "52.78.36.85")
 
-    MIDDLEWARE += (
-        'debug_toolbar.middleware.DebugToolbarMiddleware',
-    )
+# 이 코드는 장고 디버그 툴바가 안보이는 문제를 피하기 위해 추가되었습니다.
+# Failed to load module script: The server responded with a non-JavaScript MIME type of "text/plain".
+# Strict MIME type checking is enforced for module scripts per HTML spec in django tool bar
+import mimetypes
 
-    # 이 코드는 장고 디버그 툴바가 안보이는 문제를 피하기 위해 추가되었습니다.
-    # Failed to load module script: The server responded with a non-JavaScript MIME type of "text/plain".
-    # Strict MIME type checking is enforced for module scripts per HTML spec in django tool bar
-    import mimetypes
-    if mimetypes.guess_type("toolbar.js")[0] != "application/javascript":
-        print("MIME : {0}".format(mimetypes.guess_type("toolbar.js")))
-        mimetypes.add_type("application/javascript", ".js", True)
-    else:
-        print("MIME : application/javascript")
+if mimetypes.guess_type("toolbar.js")[0] != "application/javascript":
+    print("MIME : {0}".format(mimetypes.guess_type("toolbar.js")))
+    mimetypes.add_type("application/javascript", ".js", True)
 else:
-    ALLOWED_HOSTS += (
-        '127.0.0.1',
-    )
+    print("MIME : application/javascript")
