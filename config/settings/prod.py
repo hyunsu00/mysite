9c48706bdd4d1f59578906529d83b6d47a4b8996
hyunsu00 <hyun955807@naver.com>
2021-03-18 19:50:36 +0900

    admin 정적파일복사 및 디버그모드=False

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -10,3 +10,6 @@ STATIC_ROOT = BASE_DIR / 'static/'
 # base.py 파일에 STATICFILES_DIRS 항목이 이미 있는데 prod.py 파일에 다시 빈 값으로 설정하는 이유는 STATIC_ROOT가 설정된 경우 
 # STATICFILES_DIRS 리스트에 STATIC_ROOT와 동일한 디렉터리가 포함되어 있으면 서버 실행 시 다음과 같은 오류가 발생하기 때문이다.
 STATICFILES_DIRS = []
+
+DEBUG = False
+
